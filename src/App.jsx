cfb6109f2c1e4e45207dde2ecabfa3d0735a994d
JCDiaz1201 <jcdiaz1201git@gmail.com>
2024-07-23 16:23:26 -0500

    [tir-1]: Made app and Sidebar components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
+import React from 'react';
+
+import Sidebar from './components/sidebar/Sidebar';
+
+import "./App.css";
+
 function App() {
   return (
-    <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-      <main>
-        <h2>Time to get started!</h2>
-      </main>
+    <div className={"app"}>
+        <Sidebar />
     </div>
   );
 }
